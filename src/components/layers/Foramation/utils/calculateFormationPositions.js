59a60ee3c2799f125df64e78fd7715820bf43cb2
PitsianisParaskevas <paris.pitsianis@gmail.com>
2025-04-23 22:44:22 +0200

    fix GK position

--- src/components/layers/Foramation/utils/calculateFormationPositions.js
+++ src/components/layers/Foramation/utils/calculateFormationPositions.js
@@ -9,42 +9,66 @@ export function calculateFormationPositions({
   formationWidthRatio = 1,
 }) {
   const formationParts = formation.split("-").map(Number);
-  formationParts.unshift(1); // add goalkeeper
+  formationParts.unshift(1); // Add GK at the beginning
 
-  // Directional logic
+  // Define pitch geometry based on direction
   const pitchLength = direction === "horizontal" ? width : height;
   const pitchWidth = direction === "horizontal" ? height : width;
 
   const semiLength = pitchLength / 2;
   const usableLength = semiLength * formationWidthRatio;
-  const offsetX = (semiLength - usableLength) / 2;
-  const startX = isHomeTeam ? 0 : semiLength;
+  const offset = (semiLength - usableLength) / 2;
+  const startLine = isHomeTeam ? 0 : semiLength;
 
-  const playerWidth = 20;
-  const playerHeight = 20;
+  const playerRadius = 10;
+  const playerWidth = playerRadius * 2;
+  const playerHeight = playerRadius * 2;
+
   const areaWidth = usableLength / formationParts.length;
 
-  // 🧠 Flip order if away team
+  // Flip formation for away team (so we render from goal to attack)
   const zones = isHomeTeam ? formationParts : [...formationParts].reverse();
 
   const playerPositions = [];
 
-  zones.forEach((numPlayers, i) => {
-    const rawX =
-      startX + offsetX + (i * areaWidth + areaWidth / 2) - playerWidth / 2;
+  for (let i = 0; i < zones.length; i++) {
+    const numPlayers = zones[i];
+
+    // x = along pitchLength (horizontal: x, vertical: y)
+    const logicalX =
+      startLine + offset + (i * areaWidth + areaWidth / 2) - playerWidth / 2;
+
+    // space players evenly along pitchWidth (horizontal: y, vertical: x)
     const spaceBetween =
       (pitchWidth - numPlayers * playerHeight) / (numPlayers + 1);
 
     for (let j = 0; j < numPlayers; j++) {
-      const rawY = j * (playerHeight + spaceBetween) + spaceBetween;
-      const p = applyDirection(rawX, rawY, direction, pitchLength, pitchWidth);
+      const logicalY = j * (playerHeight + spaceBetween) + spaceBetween;
+
+      // Center before applying direction
+      const centeredX = logicalX + playerWidth / 2;
+      const centeredY = logicalY + playerHeight / 2;
+
+      const p = applyDirection(
+        centeredX,
+        centeredY,
+        direction,
+        pitchLength,
+        pitchWidth
+      );
+
+      // GK detection: first player in home, last in away (because we reversed)
+      const isGK = isHomeTeam
+        ? i === 0 && j === 0
+        : i === zones.length - 1 && j === 0;
+
       playerPositions.push({
-        x: p.x + playerWidth / 2,
-        y: p.y + playerHeight / 2,
-        isGK: i === 0 && j === 0,
+        x: p.x,
+        y: p.y,
+        isGK,
       });
     }
-  });
+  }
 
   return playerPositions;
 }

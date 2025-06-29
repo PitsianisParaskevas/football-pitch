7e998f85b1d6223849336c955859c7707b267b42
PitsianisParaskevas <paris.pitsianis@gmail.com>
2025-06-29 23:57:35 +0200

    check layers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import DrawPitch from "./components/DrawPitch/DrawPitch";
+import FormationLayer from "./components/layers/Foramation/FormationLayer";
+import HeatmapLayer from "./components/layers/HeatMap/HeatmapLayer";
+import heatmapData from "./components/layers/HeatMap/data/heatmap.json";
 
 function App() {
   return (
     <div style={{ padding: "2rem" }}>
       <h2>Custom Football Pitch</h2>
-      <DrawPitch />
+      <DrawPitch>
+        {/* <HeatmapLayer
+          data={heatmapData.heatmap}
+          width={800}
+          height={500}
+          direction="horizontal"
+          color="orange"
+          radius={10}
+          opacity={0.4}
+        /> */}
+        {/* <FormationLayer formation="4-4-2" isHomeTeam={true} />
+        <FormationLayer formation="4-3-3" isHomeTeam={false} /> */}
+      </DrawPitch>
     </div>
   );
 }
